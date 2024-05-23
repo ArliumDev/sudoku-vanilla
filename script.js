203f2814cd8019aa3ae9f678c4119aa6a4ed8ec0
ArliumDev <arliumdev@gmail.com>
2024-05-23 15:40:54 +0100

    number draw bug fixed

--- script.js
+++ script.js
@@ -5,16 +5,16 @@ let cell;
 
 for (let i = 0; i < 81; i++) {
   cell = document.createElement("button");
-  cell.classList.add("cell");
   cell.onclick = drawNumber;
+  cell.classList.add("cell");
   board.appendChild(cell);
 }
 
 for (let i = 1; i < 10; i++) {
   let key = document.createElement("button");
+  key.onclick = selectNumber;
   key.innerText = [i];
   key.classList.add("key");
-  key.onclick = selectNumber;
   keys.appendChild(key);
 }
 
@@ -23,6 +23,6 @@ function selectNumber(e) {
   console.log(selected);
 }
 
-function drawNumber() {
-  cell.innerText = selected;
+function drawNumber(e) {
+  e.target.innerText = selected;
 }
