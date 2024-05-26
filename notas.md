9e0b5c4a4b690098b3d6717d05467e1f017a42a7
ArliumDev <arliumdev@gmail.com>
2024-05-26 19:05:22 +0100

    added 'undo', 'erase' and 'notes' features. Pending bug fix

--- notas.md
+++ notas.md
@@ -1,3 +1,7 @@
 Tener en cuenta que hay que usar un array para guardar los números que se van poniendo en el tablero, tanto para el localStorage como para borrar, deshacer y el modo anotaciones
 
 Para no mutar el estado inicial del sudoku, necesito guardar el estado post-generación del mismo en un array y trabajar con copias del mismo con la feature de borrar, de deshacer y de reiniciar. 
+
+**BUG** 
+
+Al parece hay un bug al usar el eraser y luego querer usar el undo. Una vez usas el eraser, el undo deja de funcionar. Probablemente por mutación del array original. Plantear hacer un map por cada movimiento (adición o sustracción) y devolver ese array siempre para que esté disponible para las demás funciones. 
--- script.js
+++ script.js
@@ -1,57 +1,113 @@
 const board = document.getElementById('board');
 const keys = document.getElementById('keys');
-let selected;
-let cell;
 const lastMove = [];
+const tableStatus = Array(81).fill(null);
+const tableNotes = Array(81).fill(null);
+let selected = '';
+let cell;
 let erase = false;
+let notes = false;
+
+// Función que crea las casillas del tablero y las keys de los números para rellenar este.
 
 const createGameTable = () => {
   for (let i = 0; i < 81; i++) {
     cell = document.createElement('button');
-    cell.onclick = drawNumber;
     cell.classList.add('cell');
-    cell.setAttribute('value', [i]);
+    cell.setAttribute('id', [i]);
+    cell.addEventListener('click', drawNumber);
+    cell.addEventListener('click', eraser);
     board.appendChild(cell);
   }
   for (let i = 1; i < 10; i++) {
     let key = document.createElement('button');
     key.onclick = selectNumber;
     key.innerText = [i];
+    key.setAttribute('value', [i]);
     key.classList.add('key');
     keys.appendChild(key);
   }
 };
 
+// Funciones para utilizar el valor de las keys y dibujar el mismo en la casilla del tablero que se clique.
+
+const selectNumber = (e) => {
+  selected = e.target.value;
+};
+
+/* 
+
+- El primer bloque comprueba que haya alguna key seleccionada, que las anotaciones estén desactivadas y que el erase esté desactivado, quitando la clase 'notes' del valor a dibujar, dibujando el valor en la casilla clicada, y añadiendo ese cambio a la posición correspondiente del array 'tableStatus'
+
+- El segundo bloque comprueba que haya alguna key seleccionada, que las anotaciones estén activadas y que el erase esté desactivado, añadiendo la clase 'notes' al valor a dibujar, dibujando el valor en la casilla clicada con un estilo definido para diferenciar que es una anotación, y añadiendo ese cambio a la posición correspondiente del array 'tableNotes'
+
+- El tercer y último bloque simplemente retorna al no cumplirse ninguna de las anteriores condiciones, haciendo que no se escriba nada
+
+
+*/
+
+const drawNumber = (e) => {
+  if (selected !== '' && notes == false && erase == false) {
+    e.target.classList.remove('notes');
+    e.target.innerText = selected;
+    tableStatus[e.target.id] = selected;
+    console.log(tableStatus);
+    lastMove.push({ number: selected, id: e.target.id });
+  } else if (selected !== '' && notes == true && erase == false) {
+    e.target.classList.add('notes');
+    e.target.innerText = selected;
+    tableNotes[e.target.id] = selected;
+    lastMove.push({ number: selected, id: e.target.id });
+    console.log(tableNotes);
+  } else {
+    return;
+  }
+};
+
+// Funciones toggle. 'isEraserOn' hace toggle al eraser, y 'isNotesOn' hace toggle a las anotaciones
+
 const isEraserOn = () => {
   erase = !erase;
   console.log(erase);
-}
-
-const drawNumber = (e) => {
-  e.target.innerText = selected;
-  lastMove.push(e.target.innerText);
-  console.log(e.target.value);
-  console.log(lastMove);
 };
 
-const selectNumber = (e) => {
-  selected = e.target.innerText;
+const isNotesOn = () => {
+  notes = !notes;
+  console.log(notes);
 };
 
-createGameTable();
+// TODO: Arreglar bug con la función de eraser y undo. Mirar notas.
 
-const eraser = () => {
+// Funciones de eraser y undo. La primera activa el eraser y borra el número de la casilla donde se clique. La segunda deshace el último movimiento realizado en el tablero.
 
-}
+const eraser = (e) => {
+  if (erase == false) {
+    console.log("I can't erase");
+    return;
+  }
+  if (e.target.classList.contains('notes')) {
+    e.target.innerText = '';
+    tableNotes[e.target.id] = null;
+    console.log(tableNotes);
+  }
+   else {
+    console.log('I can erase');
+    e.target.innerText = '';
+    tableStatus[e.target.id] = null;
+    console.log(tableNotes);
+  }
+};
 
 const undo = () => {
-  lastMove.pop();
-  console.log(lastMove);
-}
+  if (lastMove.length > 0) {
+    const lastAdd = lastMove.pop();
+    const cell = document.querySelector(`button[id="${lastAdd.id}"]`);
+    cell.innerText = '';
+    console.log(lastMove);
+    console.log(tableNotes);
+  }
+};
 
-// const table = Array(81).fill(null);
-// const initialTable = 0;
+// Llamada a la función de creación del tablero y las keys
 
-// console.log(table);
-
-/* La función de Erase será un toggle que responda tanto a darle click para activar y desactivar, como también al darle a cualquier otra tecla (que no celda). Planteamiento en caliente: Hacer un pop a la posición del array de la celda que se quiera borrar. El undo sería con un pop pero a la última posición de un array copia que registre todos los movimientos*/
+createGameTable();
