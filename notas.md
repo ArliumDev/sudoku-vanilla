8a71a245c18b06883c92129bb15484476a1b2ea5
ArliumDev <arliumdev@gmail.com>
2024-05-29 11:18:00 +0100

    fixed array board management

--- notas.md
+++ notas.md
@@ -5,3 +5,10 @@ Para no mutar el estado inicial del sudoku, necesito guardar el estado post-gene
 **BUG** 
 
 Al parece hay un bug al usar el eraser y luego querer usar el undo. Una vez usas el eraser, el undo deja de funcionar. Probablemente por mutación del array original. Plantear hacer un map por cada movimiento (adición o sustracción) y devolver ese array siempre para que esté disponible para las demás funciones. 
+
+**IMPORTANTE**
+
+Tengo que hacer que las casillas se conecten con el array de historial para que al hacer el check de si ya había un número antes en fila y/o columna, resalte de rojo el último añadido.
+
+Recordar que hay que checkear si el número que queremos escribir ya está en la casilla, además de bloquear la casilla si ya hay un número escrito. Y más adelante, junto con el algoritmo de creación de juego aleatorio, ir bloqueando a medida que se pongan los números en las celdas correctas.
+
--- script.js
+++ script.js
@@ -1,45 +1,76 @@
-const cellBoard = document.getElementById("board");
-const keys = document.getElementById("keys");
+const board = document.getElementById('board');
+const keys = document.getElementById('keys');
 let cell;
 let key;
+let selected;
 
 // Esto hace un array de 9 índices, los cuales a su vez son un array de 9 índices. De esta manera, hemos armado 9 filas para poder comprobar si ya existe el número a escribir en esa fila, y solamente habrá que pensar cómo hacerlo con las columnas. Iterar sobre array de filas y luego el de columnas. (Perplexity)
 
-const board = Array(9).fill(null).map(() => Array(9).fill(null));
+const boardArr = Array(9)
+  .fill(null)
+  .map(() => Array(9).fill(null));
 
-console.log(board);
+console.log(boardArr);
 
 const createGameTable = () => {
   for (let i = 0; i < 81; i++) {
-    cell = document.createElement("button");
-    cell.classList.add("cell");
-    cellBoard.appendChild(cell);
-  };
-  const testing = (arr) => {
-    for (let i = 0; i < arr.length; i++) {
-      for (let j = 0; j < arr[i].length; j++) {
-        console.log([j])
-      }
-    }
+    cell = document.createElement('button');
+    cell.classList.add('cell');
+    cell.setAttribute('id', [i]);
+    cell.addEventListener('click', drawNumber);
+    board.appendChild(cell);
   }
-  testing(board);
-}
+};
 
 const createGameKeys = () => {
   for (let i = 1; i < 10; i++) {
-    key = document.createElement("button");
-    key.classList.add("key");
+    key = document.createElement('button');
+    key.classList.add('key');
+    key.setAttribute('value', [i]);
+    key.addEventListener('click', selectNumber);
     key.innerText = [i];
     keys.appendChild(key);
   }
-}
+};
 
+const rowColChecker = (board, row, col, num) => {
+  // Comprobar si ya estaba en fila
+  for (let i = 0; i < 9; i++) {
+    if (board[row][i] === num && i !== col) {
+      console.log('Ya estaba ese número en la fila');
+    }
+  }
+  // Comprobar si ya estaba número en columna
+  for (let i = 0; i < 9; i++) {
+    if (board[i][col] === num && i !== row) {
+      console.log('Ya estaba ese número en la columna');
+    }
+  }
+};
 
+const selectNumber = (e) => {
+  selected = e.target.value;
+};
+
+const drawNumber = (e) => {
+  const drawCell = e.target;
+  drawCell.innerText = selected;
+  let found = false;
+
+  for (let i = 0; i < boardArr.length; i++) {
+    for (let j = 0; j < boardArr[i].length; j++) {
+      if (parseInt(drawCell.id) === i * boardArr[i].length + j) {
+        boardArr[i][j] = drawCell.innerText;
+        console.log(boardArr);
+        found = true;
+        break;
+      }
+    }
+    if (found) {
+      break;
+    }
+  }
+};
 
 createGameTable();
 createGameKeys();
-
-console.log(cell); 
-
-
-
